35569194598cedd0ee239798efca5592a68e9d1f
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-08-21 15:06:54 +0900

    [Update]Add OVER(PARTITION BY category ORDER BY score DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS product_id to postgres/sql/3-3-2/3.3.2.6.sql

--- postgres/sql/3-3-2/3.3.2.6.sql
+++ postgres/sql/3-3-2/3.3.2.6.sql
@@ -3,6 +3,8 @@ SELECT DISTINCT
     category
     -- カテゴリごとにランキング最上位の商品IDを取得する
     , FIRST_VALUE(product_id)
-
+        OVER(PARTITION BY category ORDER BY score DESC
+          ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+      AS product_id
 FROM popular_products
 ;
