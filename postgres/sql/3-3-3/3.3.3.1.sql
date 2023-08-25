e51d33cfacd4e14b8b1f8582cbfed57ac810e323
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-08-25 11:56:47 +0900

    [Update]Add listagg(product_id, ',') AS product_ids to postgres/sql/3-3-3/3.3.3.1.sql

--- postgres/sql/3-3-3/3.3.3.1.sql
+++ postgres/sql/3-3-3/3.3.3.1.sql
@@ -4,5 +4,8 @@ SELECT
     -- 商品IDを配列に集約し、カンマ区切りの文字列に変換
     -- ■ PostgreSQL, BigQueryの場合はstring_aggを用いる
   , string_agg(product_id, ',') AS product_ids
+
+    -- ■ Redshiftの場合はlistaggを用いる
+    -- , listagg(product_id, ',') AS product_ids
 FROM daily_kpi
 ;
