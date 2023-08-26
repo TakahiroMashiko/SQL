dd8a02c9a59a84c2bbce3ba4ed6ee1f6b973aa5e
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-08-26 19:34:29 +0900

    [Update]Add concat_ws(',', collet_list(product_id)) AS product_ids to postgres/sql/3-3-3/3.3.3.1.sql

--- postgres/sql/3-3-3/3.3.3.1.sql
+++ postgres/sql/3-3-3/3.3.3.1.sql
@@ -7,5 +7,8 @@ SELECT
 
     -- ■ Redshiftの場合はlistaggを用いる
     -- , listagg(product_id, ',') AS product_ids
+
+    -- Hive, SparkSQLの場合はcollet_listとconcat_wsを用いる
+    -- , concat_ws(',', collet_list(product_id)) AS product_ids
 FROM daily_kpi
 ;
