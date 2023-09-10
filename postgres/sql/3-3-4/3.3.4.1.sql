3234aea23622d2d01dc8565a3cf8c91d3e9f5479
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-09-11 00:49:18 +0900

    [Update]Add WHEN p.idx = 4 THEN q.q4 to postgres/sql/3-3-4/3.3.4.1.sql

--- postgres/sql/3-3-4/3.3.4.1.sql
+++ postgres/sql/3-3-4/3.3.4.1.sql
@@ -12,6 +12,7 @@ SELECT
       WHEN p.idx = 1 THEN q.q1
       WHEN p.idx = 2 THEN q.q2
       WHEN p.idx = 3 THEN q.q3
+      WHEN p.idx = 4 THEN q.q4
     END AS sales
 FROM
     quarterly_sales AS q
