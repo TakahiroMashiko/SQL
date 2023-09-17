b5fd09bef0a83bc63bcf5ae671c1828588632070
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-09-18 08:08:55 +0900

    [Update]Add SELECT * FROM unnest(ARRAY['A001','A002,'A003]) AS product_id; to postgres/sql/3-3-4/3.3.4.2.sql

--- postgres/sql/3-3-4/3.3.4.2.sql
+++ postgres/sql/3-3-4/3.3.4.2.sql
@@ -1,2 +1,5 @@
 -- ■ PostgreSQLの場合、unnest関数を用いる
 SELECT unnest(ARRAY['A001','A002,'A003]) AS product_id;
+
+-- ■ BigQueryの場合もunnest関数を用いるが、テーブル関数はFROM句内でしか使えない
+SELECT * FROM unnest(ARRAY['A001','A002,'A003]) AS product_id;
