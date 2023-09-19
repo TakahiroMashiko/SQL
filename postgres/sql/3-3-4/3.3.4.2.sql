cc47644a5fd8e4044d14a38b78be4176c097cd14
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-09-19 12:01:13 +0900

    [Update]Add SELECT explode(ARRAY('A001','A002','A003')) AS product_id; to postgres/sql/3-3-4/3.3.4.2.sql

--- postgres/sql/3-3-4/3.3.4.2.sql
+++ postgres/sql/3-3-4/3.3.4.2.sql
@@ -3,3 +3,6 @@ SELECT unnest(ARRAY['A001','A002,'A003]) AS product_id;
 
 -- ■ BigQueryの場合もunnest関数を用いるが、テーブル関数はFROM句内でしか使えない
 SELECT * FROM unnest(ARRAY['A001','A002,'A003]) AS product_id;
+
+-- ■ Hive, SparkSQLの場合、explode関数を用いる
+-- SELECT explode(ARRAY('A001','A002','A003')) AS product_id;
