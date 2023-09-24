154684d9eed76bdcb4cfc4ad9dfa5a164e9dcaac
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-09-24 09:54:28 +0900

    [Update]Add CROSS JOIN unnest(split(product_ids, ',')) AS product_id to postgres/sql/3-3-4/3.3.4.3.sql

--- postgres/sql/3-3-4/3.3.4.3.sql
+++ postgres/sql/3-3-4/3.3.4.3.sql
@@ -5,3 +5,6 @@ FROM
   purchase_log AS p
   -- string_to_array関数で文字列を配列に変換し、unnest関数でテーブルに変換する
   CROSS JOIN unnest(string_to_array(product_ids, ',')) AS product_id
+
+  -- BigQueryの場合は、文字列分解にsplit関数を用いる
+  -- CROSS JOIN unnest(split(product_ids, ',')) AS product_id
