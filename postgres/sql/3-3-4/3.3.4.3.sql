403c3006ffdedf8151a30f9fb2650e4a3c4a12ea
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-09-25 08:39:22 +0900

    [Update]Add LATERAL VIEW explode(split(product_ids, ',')) e AS product_id; to postgres/sql/3-3-4/3.3.4.3.sql

--- postgres/sql/3-3-4/3.3.4.3.sql
+++ postgres/sql/3-3-4/3.3.4.3.sql
@@ -6,5 +6,8 @@ FROM
   -- string_to_array関数で文字列を配列に変換し、unnest関数でテーブルに変換する
   CROSS JOIN unnest(string_to_array(product_ids, ',')) AS product_id
 
-  -- BigQueryの場合は、文字列分解にsplit関数を用いる
+  -- ■ BigQueryの場合は、文字列分解にsplit関数を用いる
   -- CROSS JOIN unnest(split(product_ids, ',')) AS product_id
+
+  -- ■ HiveやSparkSQLの場合は、LATERAL VIEW explodeを用いる
+  -- LATERAL VIEW explode(split(product_ids, ',')) e AS product_id;
