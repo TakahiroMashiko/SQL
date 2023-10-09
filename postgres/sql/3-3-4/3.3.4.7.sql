2d0fda4eec2fb1a63953f8f50f6c0d55062e5d3e
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-10-09 13:33:31 +0900

    [Update]Add - char_length(replace(product_ids, ',', '')) AS product_num to postgres/sql/3-3-4/3.3.4.7.sql

--- postgres/sql/3-3-4/3.3.4.7.sql
+++ postgres/sql/3-3-4/3.3.4.7.sql
@@ -3,6 +3,8 @@ SELECT
   , product_ids
   -- 商品IDの文字列からカンマを取り除き、文字列の差を計算して商品数を取得する
   , 1 + char_length(product_ids)
+      - char_length(replace(product_ids, ',', ''))
+  AS product_num
 FROM
   purchase_log
 ;
