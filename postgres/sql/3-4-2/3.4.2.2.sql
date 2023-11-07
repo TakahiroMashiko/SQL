d2d43cbc4cce89f78ac9bbfeb32062dda915a812
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-11-07 11:54:43 +0900

    [Update]Add LEFT JOIN category_sales AS s to postgres/sql/3-4-2/3.4.2.2.sql

--- postgres/sql/3-4-2/3.4.2.2.sql
+++ postgres/sql/3-4-2/3.4.2.2.sql
@@ -2,6 +2,10 @@ SELECT
   m.category_id
   , m.name
   , s.sales
-  , r.product_id AS sale_product
+  , r.product_id AS top_sale_product
 FROM
   mst_categories AS m
+-- 右外部結合を用いて結合できなかったレコードを残す
+LEFT JOIN
+  -- カテゴリー別の売上額を結合する
+  category_sales AS s
