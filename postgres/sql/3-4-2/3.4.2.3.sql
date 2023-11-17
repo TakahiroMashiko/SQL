ef9021bb3f96722dc8c2d5a1e322ca9ae0cbb55f
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-11-17 21:36:19 +0900

    [Update]Add WHERE m.category_id = s.category_id to postgres/sql/3-4-2/3.4.2.3.sql

--- postgres/sql/3-4-2/3.4.2.3.sql
+++ postgres/sql/3-4-2/3.4.2.3.sql
@@ -3,6 +3,7 @@ SELECT
   , m.name
   -- 相関サブクエリでカテゴリー別の売上額を取得
   (SELECT s.sales
-   FROM category_sales AS s)
+   FROM category_sales AS s
+   WHERE m.category_id = s.category_id)
 FROM
   mst_categories AS m
