d9dbbe0de8f47cc39972d0af78778566b31f2bb7
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-11-18 10:15:21 +0900

    [Update]Add (SELECT r.product_id) AS top_sale_product to postgres/sql/3-4-2/3.4.2.3.sql

--- postgres/sql/3-4-2/3.4.2.3.sql
+++ postgres/sql/3-4-2/3.4.2.3.sql
@@ -2,8 +2,12 @@ SELECT
   m.category_id
   , m.name
   -- 相関サブクエリでカテゴリー別の売上額を取得
-  (SELECT s.sales
+  , (SELECT s.sales
    FROM category_sales AS s
-   WHERE m.category_id = s.category_id)
+   WHERE m.category_id = s.category_id
+   ) AS sales
+   -- 相関サブクエリでカテゴリー別のトップ売上商品を1件取得（ランクによる絞込が不要）
+   , (SELECT r.product_id
+   ) AS top_sale_product
 FROM
   mst_categories AS m
