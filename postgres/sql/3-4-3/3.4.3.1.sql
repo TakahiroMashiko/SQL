ec7a9eb2ef2dd757e536a19d7bc1be6eef18b345
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-11-28 11:57:29 +0900

    [Update]Add CASE WHEN m.card_number IS NOT NULL THEN 1 ELSE 0 END AS has_card to postgres/sql/3-4-3/3.4.3.1.sql

--- postgres/sql/3-4-3/3.4.3.1.sql
+++ postgres/sql/3-4-3/3.4.3.1.sql
@@ -2,5 +2,7 @@ SELECT
   m.user_id
   , m.card_number
   , COUNT(p.user_id) AS purchase_count
+  -- クレジットカード番号を登録している場合は1、登録していない場合は0で表現する
+  , CASE WHEN m.card_number IS NOT NULL THEN 1 ELSE 0 END AS has_card
 FROM
   mst_users_with_card_number AS m
