16ffa5b612d88f401e2c1630213af8b02ace690d
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2023-11-29 13:47:09 +0900

    [Update]Add SIGN(COUNT(p.user_id)) AS has_purchased to postgres/sql/3-4-3/3.4.3.1.sql

--- postgres/sql/3-4-3/3.4.3.1.sql
+++ postgres/sql/3-4-3/3.4.3.1.sql
@@ -4,5 +4,7 @@ SELECT
   , COUNT(p.user_id) AS purchase_count
   -- クレジットカード番号を登録している場合は1、登録していない場合は0で表現する
   , CASE WHEN m.card_number IS NOT NULL THEN 1 ELSE 0 END AS has_card
+  -- 購入履歴がある場合は1、ない場合は0で表現する
+  , SIGN(COUNT(p.user_id)) AS has_purchased
 FROM
   mst_users_with_card_number AS m
