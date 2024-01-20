25d9a888f7e5fdd6e6a25474870a3e3225da6722
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-01-20 17:16:28 +0900

    [Update]Add UNION ALL SELECT 3 AS device_id, 'アプリ' AS device_name to postgres/sql/3-4-5/3.4.5.1.sql

--- postgres/sql/3-4-5/3.4.5.1.sql
+++ postgres/sql/3-4-5/3.4.5.1.sql
@@ -2,7 +2,7 @@ WITH
 mst_devices as (
   SELECT 1 AS device_id, 'PC' AS device_name
   UNION ALL SELECT 2 AS device_id, 'SP' AS device_name
-
+  UNION ALL SELECT 3 AS device_id, 'アプリ' AS device_name
 )
 
 ;
