236b3a095ca230afb6431ac3d4b4bfb19c0b32db
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-02-18 00:24:35 +0900

    [Update]Add (SELECT explode() AS t to postgres/sql/3-4-5/3.4.4.5.sql

--- postgres/sql/3-4-5/3.4.5.5.sql
+++ postgres/sql/3-4-5/3.4.5.5.sql
@@ -4,4 +4,9 @@ mst_devices AS (
     -- map型のデータを絡むに展開する
     d['device_id'] AS device_id
     , d['device_name'] AS device_name
+    FROM
+    -- 配列をテーブルに展開する
+    (SELECT explode(
+
+    ) AS t
 )
