d748fbe9424bd67235b3e0bcdd1b910cffda836d
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-02-27 00:19:19 +0900

    [Update]Add SELECT idx FROM unnest(generate_array(1, 5)) AS idx to postgres/sql/3-4-5/3.4.5.6.sql

--- postgres/sql/3-4-5/3.4.5.6.sql
+++ postgres/sql/3-4-5/3.4.5.6.sql
@@ -3,4 +3,6 @@ series AS (
   -- 1から5までの連番を生成する
   -- ■ PostgreSQLの場合、generate_seriesを用いる
   SELECT generate_series(1, 5) AS idx
+  -- ■ BigQueryの場合、generate_arrayを用いる
+  -- SELECT idx FROM unnest(generate_array(1, 5)) AS idx
 )
