d01a1f450e75147201868bc953b2c983f9932215
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-03-25 01:24:51 +0900

    [Update]Add , substr(dt, 1, 7) AS year_month to postgres/sql/4-1-3/4.1.3.1.sql

--- postgres/sql/4-1-3/4.1.3.1.sql
+++ postgres/sql/4-1-3/4.1.3.1.sql
@@ -3,6 +3,8 @@ SELECT
   -- 「年-月」を抽出する
   -- ■ PostgreSQL, Hive, Redshift, SparkSQLの場合、substringで「年-月」部分を抽出する
   , substring(dt, 1, 7) AS year_month
+  -- ■ PostgreSQL, Hive, BigQuery, SparkSQLの場合、substrが利用できる
+  -- , substr(dt, 1, 7) AS year_month
 FROM
   purchase_log
 ;
