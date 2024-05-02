3bc8bea18ac8dbc1795c0bb3310db95113c6579d
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-05-02 10:11:51 +0900

    [Update]Add , substring(dt, 1, 4) AS year to postgres/sql/4-1-4/4.1.4.1.sql

--- postgres/sql/4-1-4/4.1.4.1.sql
+++ postgres/sql/4-1-4/4.1.4.1.sql
@@ -1,5 +1,11 @@
 WITH
 daily_purchase AS (
+  SELECT
+    dt
+    -- 「年」「月」「日」をそれぞれ抽出する
+    -- ■ PostgreSQL, Hive, Redshift, SparkSQLの場合は下記
+    -- ■ BigQueryの場合、substringをsubstrに修正
+  , substring(dt, 1, 4) AS year
 )
 
 ;
