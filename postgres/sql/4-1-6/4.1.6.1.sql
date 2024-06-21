607cda125b984cc99fdcbb3a751c978ce25bf914
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-06-22 02:36:47 +0900

    [Update]Add , SUM(purchase_amount) AS purchase_amount to postgres/sql/4-1-6/4.1.6.1.sql

--- postgres/sql/4-1-6/4.1.6.1.sql
+++ postgres/sql/4-1-6/4.1.6.1.sql
@@ -8,5 +8,6 @@ daily_purchase AS (
       , substring(dt, 1, 4) AS year
       , substring(dt, 6, 2) AS month
       , substring(dt, 9, 2) AS date
+      , SUM(purchase_amount) AS purchase_amount
     FROM purchase_log
 )
