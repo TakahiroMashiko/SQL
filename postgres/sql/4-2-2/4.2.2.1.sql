3d0ce62cbf1c222e806511a1984464e46072b2b8
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-09-19 10:01:27 +0900

    [Update]Add , 100.0 * amount / SUM(amount) OVER() AS composition_ratio to postgres/sql/4-2-2/4.2.2.1.sql

--- postgres/sql/4-2-2/4.2.2.1.sql
+++ postgres/sql/4-2-2/4.2.2.1.sql
@@ -16,6 +16,9 @@ monthly_sales AS (
   SELECT
       category1
     , amount
+
+    -- 構成比:　100.0 * 項目別売上 / 全体売上
+    , 100.0 * amount / SUM(amount) OVER() AS composition_ratio
   FROM
     monthly_sales
 )
