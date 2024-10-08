809852d1664661e05fad28e9cba99e5bb03c11c2
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-10-08 11:02:24 +0900

    [Update]Add , substring(dt, 1, 4) AS year  to postgres/sql/4-2-3/4.2.3.1.sql

--- postgres/sql/4-2-3/4.2.3.1.sql
+++ postgres/sql/4-2-3/4.2.3.1.sql
@@ -3,5 +3,8 @@ daily_category_amount as (
   SELECT
       dt
     , category
+    -- ▪️ PostgreSQL, Hive, Redshift, SparkSQLの場合は下記
+    -- ▪️ Bigqueryの場合、substringをsubstrに修正
+    , substring(dt, 1, 4) AS year
   FROM purchase_detail_log
 )
