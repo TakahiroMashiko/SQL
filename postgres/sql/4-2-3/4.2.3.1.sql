0cbff177c85acb17da5933d265f61bac4d96d0e9
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-10-10 11:53:05 +0900

    [Update]Add , substring(dt, 9, 2) AS date to postgres/sql/4-2-3/4.2.3.1.sql

--- postgres/sql/4-2-3/4.2.3.1.sql
+++ postgres/sql/4-2-3/4.2.3.1.sql
@@ -7,5 +7,6 @@ daily_category_amount as (
     -- ▪️ Bigqueryの場合、substringをsubstrに修正
     , substring(dt, 1, 4) AS year
     , substring(dt, 6, 2) AS month
+    , substring(dt, 9, 2) AS date
   FROM purchase_detail_log
 )
