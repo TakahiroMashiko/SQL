a1a9d793d5f8d44fbd13558fff55a42d5e203b68
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-10-20 19:02:02 +0900

    [Update]Add concat(concat(year, '-'), month) AS year_month to postgres/sql/4-2-3/4.2.3.1.sql

--- postgres/sql/4-2-3/4.2.3.1.sql
+++ postgres/sql/4-2-3/4.2.3.1.sql
@@ -15,6 +15,8 @@ daily_category_amount as (
 , monthly_category_amount as (
   SELECT
     concat(year, '-', month) AS year_month
+    -- ■ Redshiftの場合はconcat関数を組み合わせるか、||演算子を用いる
+    -- concat(concat(year, '-'), month) AS year_month
   FROM
     daily_category_amount
 )
