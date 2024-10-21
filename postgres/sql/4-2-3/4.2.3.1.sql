59d19fffae26802825ef66144aac0e310b381868
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-10-22 02:15:23 +0900

    [Update]Add year || '-' || month AS year_month to postgres/sql/4-2-3/4.2.3.1.sql

--- postgres/sql/4-2-3/4.2.3.1.sql
+++ postgres/sql/4-2-3/4.2.3.1.sql
@@ -17,6 +17,7 @@ daily_category_amount as (
     concat(year, '-', month) AS year_month
     -- ■ Redshiftの場合はconcat関数を組み合わせるか、||演算子を用いる
     -- concat(concat(year, '-'), month) AS year_month
+    -- year || '-' || month AS year_month
   FROM
     daily_category_amount
 )
