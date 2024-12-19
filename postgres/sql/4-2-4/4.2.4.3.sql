f0bf804760346c5e3d5b432b47798251fd1614e4
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2024-12-19 16:09:54 +0900

    [Update]Add , MAX(price) + 1 - MIN(price) AS range_price to postgres/sql/4-2-4/4.2.4.3.sql

--- postgres/sql/4-2-4/4.2.4.3.sql
+++ postgres/sql/4-2-4/4.2.4.3.sql
@@ -5,6 +5,8 @@ SELECT
     MAX(price) + 1 AS max_price
     -- 金額の最小値
   , MIN(price) AS min_price
+    -- 金額の範囲 + 1 (実数)
+  , MAX(price) + 1 - MIN(price) AS range_price
 FROM
   purchase_detail_log
 )
