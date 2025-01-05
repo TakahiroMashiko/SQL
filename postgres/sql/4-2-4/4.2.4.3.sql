aebfdf4889eb60d55ec0775d157cb3b32035fb28
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-01-05 13:00:02 +0900

    [Fix]Fix indent to postgres/sql/4-2-4/4.2.4.3.sql

--- postgres/sql/4-2-4/4.2.4.3.sql
+++ postgres/sql/4-2-4/4.2.4.3.sql
@@ -1,15 +1,15 @@
 WITH stats AS (
-SELECT
-      -- 金額の最大値 + 1
-      MAX(price) + 1 AS max_price
-      -- 金額の最小値
-    , MIN(price) AS min_price
-      -- 金額の範囲 + 1 (実数)
-    , MAX(price) + 1 - MIN(price) AS range_price
-      -- 階級数
-    , 10 AS bucket_num
-FROM
-    purchase_detail_log
+    SELECT
+          -- 金額の最大値 + 1
+          MAX(price) + 1 AS max_price
+          -- 金額の最小値
+        , MIN(price) AS min_price
+          -- 金額の範囲 + 1 (実数)
+        , MAX(price) + 1 - MIN(price) AS range_price
+          -- 階級数
+        , 10 AS bucket_num
+    FROM
+        purchase_detail_log
 )
 , purchase_log_with_bucket AS (
     SELECT
