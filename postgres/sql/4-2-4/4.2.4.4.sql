4400ef6deee2e2b5af6a0a700dcff741fb8b23aa
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-01-10 09:58:21 +0900

    [Update]Add , 10 AS bucket_num to postgres/sql/4-2-4/4.2.4.4.sql

--- postgres/sql/4-2-4/4.2.4.4.sql
+++ postgres/sql/4-2-4/4.2.4.4.sql
@@ -6,6 +6,8 @@ WITH stats AS (
         , MIN(price) AS min_price
           -- 金額の範囲 + 1 (実数)
         , MAX(price) + 1 - MIN(price) AS range_price
+          -- 階級数
+        , 10 AS bucket_num
     FROM
         purchase_detail_log
 )
