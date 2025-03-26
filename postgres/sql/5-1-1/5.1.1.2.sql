e2cd1d388c5b5c67a63854e4cd7e1bf7a3bef516
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-03-26 16:33:21 +0900

    [Update]Add , CASE WHEN COALESCE(user_id, '') <> '' THEN 'login' ELSE 'guest' END AS login_status to postgres/sql/5-1-1/5.1.1.2.sql

--- postgres/sql/5-1-1/5.1.1.2.sql
+++ postgres/sql/5-1-1/5.1.1.2.sql
@@ -4,6 +4,9 @@ action_log_with_status as (
         seesion
         , user_id
         , action
+        -- user_idがnullまたは空文字でない場合はloginと判定
+        , CASE WHEN COALESCE(user_id, '') <> '' THEN 'login' ELSE 'guest' END
+          AS login_status
     FROM
         action_log
 )
