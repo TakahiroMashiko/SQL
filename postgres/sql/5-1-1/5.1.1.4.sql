041364a8e42f0cd03204d06aa65b73c3f82e8efd
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-04-21 15:36:11 +0900

    [Update]Add , CASE END AS member_status to postgres/sql/5-1-1/5.1.1.4

--- postgres/sql/5-1-1/5.1.1.4.sql
+++ postgres/sql/5-1-1/5.1.1.4.sql
@@ -3,6 +3,11 @@ WITH action_log_with_status AS (
           seesion
         , user_id
         , action
+        -- ログをタイムスタンプ順に並べ、一度でもログインしたことのあるセッションの場合、
+        -- それ以降のログのステータスをmemberとする
+        , CASE
+
+          END AS member_status
     FROM
         action_log
 )
