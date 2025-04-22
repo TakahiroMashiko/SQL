90cff423f6d4b9028beec8079afa855ee1612bd5
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-04-22 13:39:34 +0900

    [Update]Add WHEN COALESCE(MAX(user_id) , '') <> '' to postgres/sql/5-1-1/5.1.1.4

--- postgres/sql/5-1-1/5.1.1.4.sql
+++ postgres/sql/5-1-1/5.1.1.4.sql
@@ -6,7 +6,9 @@ WITH action_log_with_status AS (
         -- ログをタイムスタンプ順に並べ、一度でもログインしたことのあるセッションの場合、
         -- それ以降のログのステータスをmemberとする
         , CASE
-
+            WHEN
+              COALESCE(MAX(user_id)
+                , '') <> ''
           END AS member_status
     FROM
         action_log
