e2472bd989988562156b9be8522593b89d8cabe6
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-04-26 19:15:46 +0900

    [Update]Add THEN 'member' to postgres/sql/5-1-1/5.1.1.4

--- postgres/sql/5-1-1/5.1.1.4.sql
+++ postgres/sql/5-1-1/5.1.1.4.sql
@@ -11,6 +11,8 @@ WITH action_log_with_status AS (
                 OVER(PARTITION BY seesion ORDER BY stamp
                   ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
                 , '') <> ''
+              THEN 'member'
+            
           END AS member_status
     FROM
         action_log
