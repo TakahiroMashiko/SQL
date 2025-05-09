80c6a11ca6899a28c08179a33ff47ff073cd54c7
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-05-09 10:06:36 +0900

    [Update]Add when lag (s_log.log_time) over (partition by s_log.user_id order by s_log.log_time) is null to postgres/sample.sql

--- postgres/sql/sample.sql
+++ postgres/sql/sample.sql
@@ -6,7 +6,8 @@ with search_log as (
     select
         s_log.user_id
         , case
-
+            when
+                lag (s_log.log_time) over (partition by s_log.user_id order by s_log.log_time) is null
         end as session_delta
     from
         activity.search_log s_log
