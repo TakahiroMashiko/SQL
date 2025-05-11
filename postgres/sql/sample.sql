8e46ed5b103bda64c7800b3e9ac6bc8795d05be0
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-05-11 17:20:51 +0900

    [Update]Add or s_log.log_time > lag(s_log.log_time) over (partition by s_log.user_id order by s_log.log_time) + interval '00:30'  to postgres/sample.sql

--- postgres/sql/sample.sql
+++ postgres/sql/sample.sql
@@ -1,13 +1,15 @@
 /*
 検索ログの前処理
 PK: user_id, log_time
-*/
+*/.
 with search_log as (
     select
         s_log.user_id
         , case
             when
                 lag (s_log.log_time) over (partition by s_log.user_id order by s_log.log_time) is null
+                    or s_log.log_time > lag(s_log.log_time) over (partition by s_log.user_id order by s_log.log_time) + interval '00:30' 
+            else 0
         end as session_delta
     from
         activity.search_log s_log
