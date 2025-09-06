eaf9a705e1a3cd4505b0da0c8c0f1e5fe2d41e76
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2025-09-06 23:07:12 +0900

    [Update]Add -- , CAST(replace(substr(birthdate, 1, 10), '-', '') AS int64) AS int_birth_date to postgres/sql/5-1-2/5.1.2.2

--- postgres/sql/5-1-2/5.1.2.2.sql
+++ postgres/sql/5-1-2/5.1.2.2.sql
@@ -7,6 +7,8 @@ mst_users_with_int_birth_date AS (
          -- 文字列の生年月日を、日付の整数表現に変換
         -- PostgreSQL、Redshiftの場合は下記
         , CAST(replace(substring(birth_date, 1, 10), '-', '') AS integer) AS int_birth_date
+        -- BigQueryの場合は下記
+        -- , CAST(replace(substr(birthdate, 1, 10), '-', '') AS int64) AS int_birth_date
     FROM
         mst_users
 )
